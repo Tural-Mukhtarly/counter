ddcfbc002009963ed0cc59f055c1c8fe3f577527
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-05-12 03:25:52 +0300

    10 commit
    counter Style

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,7 +1,8 @@
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useTypeSelector } from '../../redux/useTypeSelector';
+import s from './Counter.module.css'
 
 function Counter() {
     const dispatch = useDispatch()
@@ -10,16 +11,39 @@ function Counter() {
     function incCash() {
         dispatch({ type: "ADD_VALUE", payload: 1 })
     }
-    
+
     function decCash() {
         dispatch({ type: "DECREMENT_CASH", payload: 1 })
     }
-    
+
+    function disabled() {
+        if (value === 0) {
+            return true
+        }
+    }
+
     return (
-        <div>
-            <div>{value}</div>
-            <Button variant="outlined" color="primary" onClick={() => incCash()}>+</Button>
-            <Button variant="outlined" color="primary" onClick={() => decCash()}>-</Button>
+        <div className={s.counterContainer}>
+            
+            <div className={s.sm}>
+                <div className={s.startBlock}>
+                    <TextField placeholder="Start"></TextField>
+                    <Button variant="outlined" color="primary">Start</Button>
+                </div>
+                <div className={s.maxBlock}>
+                    <TextField placeholder="Max"></TextField>
+                    <Button variant="outlined" color="primary">Max</Button>
+                </div>
+            </div>
+
+            <div className={s.counter}>
+                <div className={s.value}>
+                    {value}
+                </div>
+                <Button variant="contained" color="primary" onClick={() => incCash()} >+</Button>
+                <Button variant="contained" color="secondary" onClick={() => decCash()} disabled={disabled()}>-</Button>
+            </div>
+
         </div>
     )
 }

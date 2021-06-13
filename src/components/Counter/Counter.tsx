567ec63393c970c66fe97b4e23c8bb319e2fc7b3
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-06-13 21:11:28 +0300

    12 commit

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,38 +1,54 @@
 import { Button, TextField } from '@material-ui/core';
 import React, { useState, ChangeEvent } from 'react'
 import { useDispatch } from 'react-redux';
+import { decValueAC, incValueAC } from '../../redux/store';
 import { useTypeSelector } from '../../redux/useTypeSelector';
 import s from './Counter.module.css'
 
 function Counter() {
+
     const dispatch = useDispatch()
+
     const valueStart = useTypeSelector(state => state.start)
     const valueMax = useTypeSelector(state => state.max)
 
-    const [start, setStart] = useState<number>(5)
+
+    const [start, setStart] = useState<number>(0)
     const [max, setMax] = useState<number>(0)
 
-    const onChangeNewValue = (e: ChangeEvent<HTMLInputElement>) => setStart(e.currentTarget.valueAsNumber)
+    const onChangeNewValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setStart(e.currentTarget.valueAsNumber)
+    }
     const onChangemaxValue = (e: ChangeEvent<HTMLInputElement>) => setMax(e.currentTarget.valueAsNumber)
 
-    const incValue = () => dispatch({ type: "ADD_VALUE", payload: 1 })
-    const decValue = () => dispatch({ type: "DECREMENT_CASH", payload: 1 })
+    const incValue = () => dispatch(incValueAC(1))
+    const decValue = () => dispatch(decValueAC(1))
 
     const addValue = () => {
         dispatch({ type: "START_VALUE", payload: start })
         dispatch({ type: "MAX_VALUE", payload: max })
     }
 
-    const disabledMin = () => valueStart === 0 ? true : false
+    const disabledMin = () => valueStart === start ? true : false
     const disabledMax = () => valueStart === valueMax ? true : false
-
+    const disabledSet = () => {
+        if (start < 0) {
+            return true
+        }
+        if (max < 0) {
+            return true
+        }
+        if (start === max) {
+            return true
+        }
+    }
     return (
         <div className={s.counterContainer}>
             <div className={s.sm}>
                 <div className={s.startBlock}>
                     <TextField type="number" onChange={onChangeNewValue} placeholder="Start"></TextField>
                     <TextField type="number" placeholder="Max" onChange={onChangemaxValue}></TextField>
-                    <Button onClick={addValue} variant="outlined" color="primary">SET</Button>
+                    <Button onClick={addValue} variant="outlined" color="primary" disabled={disabledSet()}>SET</Button>
                 </div>
             </div>
             <div className={s.counter}>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,28 +7,45 @@ type ActionType = {
 
 const initialState = {
     start: 0,
-    max: 2
+    max: 2,
 }
 
 export type InitialStateType = typeof initialState
 
-const reducers = (state = initialState, action: ActionType): InitialStateType => {
+const CounterReducer = (state = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
         case "START_VALUE":
             return { ...state, start: action.payload }
         case "MAX_VALUE":
             return { ...state, max: action.payload }
-        case "ADD_VALUE":
+        case "INC_VALUE":
             return { ...state, start: state.start + action.payload }
-        case "DECREMENT_CASH":
+        case "DEC_VALUE":
             return { ...state, start: state.start - action.payload }
         default:
             return state
     }
 }
 
-export type RootState = ReturnType<typeof reducers>
+export const incValueAC = (payload: number) => ({ type: "INC_VALUE", payload: payload } as const)
+export const decValueAC = (payload: number) => ({ type: "DEC_VALUE", payload: payload } as const)
 
-export const store = createStore(reducers)
+export type RootState = ReturnType<typeof CounterReducer>
+
+let preState;
+const persistedStateString = localStorage.getItem("app-state")
+if (persistedStateString) {
+    preState = JSON.parse(persistedStateString)
+}
+
+
+export const store = createStore(CounterReducer,preState)
+
+
+
+store.subscribe(() => {
+    localStorage.setItem("app-state", JSON.stringify(store.getState()))
+})
+
 
 

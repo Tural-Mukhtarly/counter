9a0f09635401d91fb71650ab0f030aa0b655dd22
Tural Mukhtarly <tmukhtarly@mail.ru>
2021-06-13 22:25:54 +0300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import Counter from './components/Counter/Counter';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import HomeCounter from './components/HomeConter';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" component={HomeCounter} />
-          <Route path="/Counter" component={Counter} />
-
-        </Switch>
-      </Router>
+      <h2>COUNTER</h2>
+      <Counter />
     </div>
   );
 }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { decValueAC, incValueAC } from '../../redux/store';
 import { useTypeSelector } from '../../redux/useTypeSelector';
 import s from './Counter.module.css'
-import { Link } from "react-router-dom";
+
 
 function Counter() {
 
@@ -17,23 +17,51 @@ function Counter() {
     const [start, setStart] = useState<number>(0)
     const [max, setMax] = useState<number>(0)
 
+    const onChangeNewValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setStart(e.currentTarget.valueAsNumber)
+    }
+    const onChangemaxValue = (e: ChangeEvent<HTMLInputElement>) => setMax(e.currentTarget.valueAsNumber)
+
     const incValue = () => dispatch(incValueAC(1))
     const decValue = () => dispatch(decValueAC(1))
 
+    const addValue = () => {
+        dispatch({ type: "START_VALUE", payload: start })
+        dispatch({ type: "MAX_VALUE", payload: max })
+    }
 
     const disabledMin = () => valueStart === start ? true : false
     const disabledMax = () => valueStart === valueMax ? true : false
-    
+    const disabledSet = () => {
+        if (start < 0) {
+            return true
+        }
+        if (max < 0) {
+            return true
+        }
+        if (start === max) {
+            return true
+        }
+    }
     return (
+        <div className={s.counterContainer}>
+            <div className={s.sm}>
+                <div className={s.startBlock}>
+                    <TextField type="number" onChange={onChangeNewValue} placeholder="Start"></TextField>
+                    <TextField type="number" placeholder="Max" onChange={onChangemaxValue}></TextField>
+                    <Button onClick={addValue} variant="outlined" color="primary" disabled={disabledSet()}>SET</Button>
+                </div>
+            </div>
             <div className={s.counter}>
                 <div className={s.value}>
                     {valueStart}
                 </div>
                 <Button variant="contained" color="primary" onClick={() => incValue()} disabled={disabledMax()}>+</Button>
                 <Button variant="contained" color="secondary" onClick={() => decValue()} disabled={disabledMin()}>-</Button>
-                <Link to="/counter">Set</Link>
+                
             </div>
+        </div >
     )
 }
 
-export default Counter;
+export default Counter
